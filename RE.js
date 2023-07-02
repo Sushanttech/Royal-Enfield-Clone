f99e5e25cd358459e866697cfcacf48842427cc9
Sushanttech <76687635+Sushanttech@users.noreply.github.com>
2023-07-02 13:17:39 +0530

    Add files via upload

---
+++ RE.js
@@ -0,0 +1,77 @@
+function a(){
+    document.getElementById("kk").src="./images/22.avif"
+}
+function a1(){
+    document.getElementById("kk").src="./images/interceptor-home-motorcycle.avif"
+}
+function b(){
+    document.getElementById("kk").src="./images/23.avif"
+}
+function c(){
+    document.getElementById("kk").src="./images/24.avif"
+}
+function d(){
+    document.getElementById("kk").src="./images/25.avif"
+}
+function e(){
+    document.getElementById("kk").src="./images/26.avif"
+}
+function f(){
+    document.getElementById("kk").src="./images/27.avif"
+}
+function g(){
+    document.getElementById("kk").src="./images/28.avif"
+}
+function h(){
+    document.getElementById("kk").src="./images/29.avif"
+}
+function i1(){
+    document.getElementById("kk1").src="./images/himalayan-odyssey-2023.avif"
+}
+function i(){
+    document.getElementById("kk1").src="./images/r2.avif"
+}
+function j(){
+    document.getElementById("kk1").src="./images/r3.avif"
+}
+function k(){
+    document.getElementById("kk1").src="./images/r4.avif"
+}
+function l(){
+    document.getElementById("kk1").src="./images/r5.avif"
+}
+function m(){
+    document.getElementById("kk1").src="./images/r6.avif"
+}
+function n1(){
+    document.getElementById("kk2").src="./images/art-of-motorcycling-apparel.avif"
+}
+function n(){
+    document.getElementById("kk2").src="./images/a2.avif"
+}
+function o(){
+    document.getElementById("kk2").src="./images/a3.avif"
+}
+function p(){
+    document.getElementById("kk2").src="./images/a4.avif"
+}
+function q(){
+    document.getElementById("kk2").src="./images/a5.avif"
+}
+function r(){
+    document.getElementById("kk2").src="./images/a6.avif"
+}
+function s(){
+    document.getElementById("kk3").src="./images/acc1.avif"
+}
+function t1(){
+    document.getElementById("kk3").src="./images/gma-home.avif"
+}
+
+function t(){
+    document.getElementById("kk4").src="./images/l2.avif"
+}
+function u(){
+    document.getElementById("kk4").src="./images/l3.avif"
+}
+
